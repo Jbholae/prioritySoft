b6f9eb55d5064bcf8463a387a29e2eb5435fee5f
Saurav <saurav.maharjan7@gmail.com>
2024-06-12 22:52:39 +0545

    Setting up project

---
+++ lib/config/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    primaryColorDark: const Color(0xff070304),
+    primaryColorLight: const Color(0xffF3F3F3),
+    scaffoldBackgroundColor: const Color(0xffE7E7E7),
+    dialogBackgroundColor: const Color(0xffB7B7B7),
+  );
+}
