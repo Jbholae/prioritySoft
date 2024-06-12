c0355321b72209c379a8ff09955ed34511b4d563
Saurav <saurav.maharjan7@gmail.com>
2024-06-13 00:28:18 +0545

    feat : Added Data - Added custom Colors and Theme Data, color of text with bold and size of font with font family

---
+++ lib/config/custom_color.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color primary500 = Color(0xff101010);
+  static const Color primary400 = Color(0xff6F6F6F);
+  static const Color primary300 = Color(0xffB7B7B7);
+  static const Color primary200 = Color(0xffE7E7E7);
+  static const Color primary100 = Color(0xffF3F3F3);
+  static const Color primary0 = Color(0xffFFFFFF);
+
+  static const Color success500 = Color(0xff52BD94);
+  static const Color error500 = Color(0xffFF4C5E);
+  static const Color warning500 = Color(0xffffbd16);
+  static const Color brand500 = Color(0xff56CCF2);
+  static const Color information500 = Color(0xff2952CC);
+  static const Color secondary500 = Color(0xffF27A47);
+}
--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -2,9 +2,78 @@ import 'package:flutter/material.dart';
 
 ThemeData theme() {
   return ThemeData(
-    primaryColorDark: const Color(0xff070304),
-    primaryColorLight: const Color(0xffF3F3F3),
-    scaffoldBackgroundColor: const Color(0xffE7E7E7),
-    dialogBackgroundColor: const Color(0xffB7B7B7),
+    primaryColor: const Color(0xff101010), // Default 500
+    scaffoldBackgroundColor: const Color(0xffE7E7E7), // Light 200
+    dialogBackgroundColor: const Color(0xffB7B7B7), // Light 300
+    fontFamily: 'Urbanist',
+    colorScheme: const ColorScheme(
+      primary: Color(0xff101010), // Primary default
+      primaryContainer: Color(0xff0D0B0B), // Primary dark 600
+      secondary: Color(0xffF27A47), // Secondary default
+      secondaryContainer: Color(0xffD05833), // Secondary dark 600
+      surface: Colors.white,
+      error: Color(0xffFF4C5E), // Error default
+      onPrimary: Colors.white,
+      onSecondary: Colors.white,
+      onSurface: Color(0xff070304),
+      onError: Colors.white,
+      brightness: Brightness.light,
+    ),
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(
+        color: Color(0xff070304),
+        fontSize: 48,
+        fontWeight: FontWeight.bold,
+      ),
+      displayMedium: TextStyle(
+        color: Color(0xff070304),
+        fontSize: 32,
+        fontWeight: FontWeight.w800,
+      ),
+      displaySmall: TextStyle(
+        color: Color(0xff070304),
+        fontSize: 24,
+        fontWeight: FontWeight.w700,
+      ),
+      headlineLarge: TextStyle(
+        color: Color(0xffB7B7B7),
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+      ),
+      headlineMedium: TextStyle(
+        color: Color(0xff070304),
+        fontSize: 18,
+        fontWeight: FontWeight.w500,
+      ),
+      headlineSmall: TextStyle(
+        color: Color(0xff070304),
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+      ),
+      titleLarge: TextStyle(
+        color: Color(0xff070304),
+        fontSize: 16,
+        fontWeight: FontWeight.w300,
+      ),
+      titleMedium: TextStyle(
+        color: Color(0xff070304),
+        fontSize: 16,
+        fontWeight: FontWeight.w200,
+      ),
+      titleSmall: TextStyle(
+        color: Color(0xff070304),
+        fontSize: 16,
+        fontWeight: FontWeight.w100,
+      ),
+      bodyLarge: TextStyle(
+        fontSize: 16,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 14,
+      ),
+      bodySmall: TextStyle(
+        fontSize: 12,
+      ),
+    ),
   );
 }
