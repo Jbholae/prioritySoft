0a0767d0f84be740d085b24e00eebab4c6955909
Saurav <saurav.maharjan7@gmail.com>
2024-06-15 00:04:01 +0545

    feat : app routes  - Added new screens case to route generator to navigate from named routes.

--- lib/config/app_routes.dart
+++ lib/config/app_routes.dart
@@ -1,4 +1,8 @@
 class AppRoutes {
   static const home = '/home';
   static const productDetails = '/product_details';
+  static const filterScreen = '/filter_screen';
+  static const reviewScreen = '/review_screen';
+  static const cartScreen = '/cart_screen';
+  static const orderSummaryScreen = '/order_summary_screen';
 }
--- lib/config/route_generator.dart
+++ lib/config/route_generator.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:prioritysoft/config/app_routes.dart';
 import 'package:prioritysoft/config/custom_color.dart';
+import 'package:prioritysoft/screen/cart_screen.dart';
+import 'package:prioritysoft/screen/filter_screen.dart';
 import 'package:prioritysoft/screen/home_screen.dart';
+import 'package:prioritysoft/screen/order_summary_screen.dart';
 import 'package:prioritysoft/screen/product_details.dart';
+import 'package:prioritysoft/screen/review_screen.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -11,6 +15,14 @@ class RouteGenerator {
         return buildRoute(const HomeScreen(), settings: settings);
       case AppRoutes.productDetails:
         return buildRoute(const ProductDetails(), settings: settings);
+      case AppRoutes.filterScreen:
+        return buildRoute(const FilterScreen(), settings: settings);
+      case AppRoutes.reviewScreen:
+        return buildRoute(const ReviewScreen(), settings: settings);
+      case AppRoutes.cartScreen:
+        return buildRoute(const CartScreen(), settings: settings);
+      case AppRoutes.orderSummaryScreen:
+        return buildRoute(const OrderSummaryScreen(), settings: settings);
       /* case AppRoutes.profile:
         final arguments = settings.arguments as ProfileArguments;
         return buildRoute(const Profile(arguments: arguments),
@@ -30,13 +42,20 @@ class RouteGenerator {
 
   static Route<dynamic> _errorRoute() {
     return MaterialPageRoute(builder: (_) {
-      return const Scaffold(
-        body: Center(
-          child: Text(
-            "Seemms the route you\'ve navigated to doesn\'t exist !!!",
-            style: TextStyle(
-              color: CustomColors.error500,
-              fontSize: 42,
+      return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Error Route'),
+        ),
+        body: const Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Center(
+            child: Text(
+              "Seems like the route you've navigated to doesn't exist !!!",
+              style: TextStyle(
+                color: CustomColors.error500,
+                fontSize: 42,
+              ),
             ),
           ),
         ),
